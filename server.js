7ca04e5afa968459f16659b4bd4d0e8e2fdc7089
freeze <fyz_seven@163.com>
2020-11-02 12:38:13 +0800

    fix

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function(req,res){
     res.writeHeader(200,{'Content-Type':'text/plain'});
     res.end('test 1337 node\n');
-}).listen(1337,'172.26.118.72');
-console.log('Server running at http:172.26.118.72:1337/')
+}).listen(1337);
+console.log('Server running at 1337/')
